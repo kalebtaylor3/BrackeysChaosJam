2ddeaecb10e082b064997114ef2b57552760b4ee
Aencro <roythomblison@gmail.com>
2021-08-22 15:50:01 -0400

    unfinished health system. healthbar not working how i wanted it to im pushing and gaming

---
+++ ChaosJam/Assets/Scripts/GameScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(100);
+
+        Debug.Log("Health: " +healthSystem.GetHealth());
+        healthSystem.Damage(10);
+        Debug.Log("Health: " + healthSystem.GetHealth());
+        healthSystem.Damage(20);
+        Debug.Log("Health: " + healthSystem.GetHealth()); 
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
