e3c8ca8196df1bd97b09e23a0e17373af25dae56
kalebtaylor3 <taylorkaleb16@gmail.com>
2021-08-23 14:15:17 -0400

    player has health

--- ChaosJam/Assets/Scripts/Player.cs
+++ ChaosJam/Assets/Scripts/Player.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-
+    public Transform HealthBar;
+    Transform healthBarTransform;
+    HealthSystem healthSystem;
     // Start is called before the first frame update
     void Start()
     {
-        
+        healthSystem = new HealthSystem(100);
+
+        healthBarTransform = Instantiate(HealthBar, new Vector2(transform.position.x, transform.position.y + 0.5f), Quaternion.identity);
+        HealthBar healthBar = healthBarTransform.GetComponent<HealthBar>();
+        healthBar.Setup(healthSystem);
     }
 
     // Update is called once per frame
