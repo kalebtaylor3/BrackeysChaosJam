0d4fc063696bdb95ff6b66e69bc5187d40fe7ee1
kalebtaylor3 <taylorkaleb16@gmail.com>
2021-08-23 13:52:02 -0400

    //

--- ChaosJam/Assets/Scripts/enemy.cs
+++ ChaosJam/Assets/Scripts/enemy.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour
 {
+
+    public Transform HealthBar;
+    Transform healthBarTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        HealthSystem healthSystem = new HealthSystem(100);
+
+        healthBarTransform = Instantiate(HealthBar, new Vector2(transform.position.x, transform.position.y + 1), Quaternion.identity);
+        HealthBar healthBar = healthBarTransform.GetComponent<HealthBar>();
+        healthBar.Setup(healthSystem);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthBarTransform.position = new Vector2(transform.position.x, transform.position.y + 1.5f);
     }
 }
